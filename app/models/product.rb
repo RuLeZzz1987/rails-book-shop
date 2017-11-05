6ae205cac4b05407448a880a0f09d4549e0699c7
Alexander Duka <alexander.duka@namecheap.com>
2017-11-05 18:52:55 +0200

    validation playground

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,10 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, presence: true
-  validates :price, numericality: { greater_than_or_equal_to: 1 }
+  validates :price, numericality: { greater_than_or_equal_to: 1,
+                                    message: 'should be greater 0!' }
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-      with: %r{\.(gif|jpg|png)\Z}i,
-      message: 'URL should be GIF, JPG or PNG.'
+    with: /\.(gif|jpg|png)\Z/i,
+    message: 'URL should be GIF, JPG or PNG.'
   }
 end
