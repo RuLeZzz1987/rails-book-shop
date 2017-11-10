fc78b88080ca70ec0482cefe94a3bcdf9e540716
Alexander Duka <alexander.duka@namecheap.com>
2017-11-10 17:57:27 +0200

    adding to cart and cart scaffold

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ActiveRecord::Base
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 1,
                                     message: 'should be greater 0!' }
@@ -11,5 +15,14 @@ class Product < ActiveRecord::Base
 
   def self.latest
     Product.order(:updated_at).last
+  end
+
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+      true
+    else
+      errors.add(:base, 'line items are not empty')
+      false
+    end
   end
 end
